b2dcc05647f24e75a8c9fa7d3ecc85dd7ed045ce
Jose Manuel Carretero <josemanuelcarreterocuenca@gmail.com>
2020-09-15 20:07:27 +0200

    Add a class to validate and normalice data generically

--- src/domain/value_objects/order.vo.js
+++ src/domain/value_objects/order.vo.js
@@ -1,6 +1,10 @@
 'use strict';
 
-module.exports = class Order {
+const streetLookup = require('./street.values');
+const stateLookup = require('./state.values');
+const ValueObject = require('./valueObject');
+
+module.exports = class Order extends ValueObject {
     constructor(
         orderId,
         dealId,
@@ -11,14 +15,40 @@ module.exports = class Order {
         zipCode,
         creditCard
     ) {
-        this.orderId = orderId;
-        this.dealId = dealId;
-        this.email = email;
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.zipCode = state;
-        this.creditCard = creditCard;
+        super();
+        this.validateValueObject('orderId', orderId, true, 'integer');
+        this.validateValueObject('dealId', dealId, true, 'integer');
+        this.validateValueObject('email', email, true, 'email', (value) => {
+            return value.toLowerCase();
+        });
+        this.validateValueObject('street', street, true, 'string', (value) => {
+            return Order.applyLookup(streetLookup, value.toLowerCase());
+        });
+        this.validateValueObject('city', city, true, 'string', (value) => {
+            return value.toLowerCase();
+        });
+        // (Fixed) the 'state' field was being assigned with the value of the 'city' field
+        /** Original
+         * // Normalize state
+         * order.state = order.street.replace('il', 'illinois').replace('ca', 'california').replace('ny', 'new york')
+         *
+         */
+        /** Then
+         * // Normalize state
+         * order.state = order.street.replace('il', 'illinois').replace('ca', 'california').replace('ny', 'new york')
+         *
+         **/
+        this.validateValueObject('state', state, true, 'string', (value) => {
+            return Order.applyLookup(stateLookup, value.toLowerCase());
+        });
+        this.validateValueObject('zipCode', zipCode, true, 'string');
+        this.validateValueObject('creditCard', creditCard, true, 'string');
+    }
+
+    static applyLookup(lookup, value) {
+        return Object.keys(lookup).reduce((currentValue, word) => {
+            return currentValue.replace(word, lookup[word]);
+        }, value);
     }
 
     static create({
---
+++ src/domain/value_objects/valueObject.js
@@ -0,0 +1,118 @@
+const validator = require('email-validator');
+
+class ValueObject {
+    static validateStringValueObject(field, value) {
+        if (typeof value !== 'string')
+            throw new Error(`the "${field}" field value must be string`);
+    }
+
+    static validateEmailValueObject(field, value) {
+        ValueObject.validateStringValueObject(field, value);
+        if (!validator.validate(value)) {
+            throw new Error(`the "${field}" field value is not valid format`);
+        }
+    }
+
+    static validateNumberValueObject(field, value) {
+        if (NaN(value)) {
+            throw new Error(`the "${field}" field value must be number`);
+        }
+    }
+
+    static validateIntegerValueObject(field, value) {
+        if (isNaN(value) || parseInt(value) !== parseFloat(value))
+            throw new Error(`the "${field}" field value must be integer`);
+    }
+
+    static validateRequiredField(field, value) {
+        if (value === undefined || value === null)
+            throw new Error(
+                `the "${field}" field value can not be undefined or null`
+            );
+    }
+
+    static normalizeEmailValueObject(value) {
+        const aux = value.split('@');
+        const atIndex = aux[0].indexOf('+');
+        // Src: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/substring
+        // Src: https://tools.ietf.org/id/draft-newman-email-subaddr-01.html#toc
+        // (Fixed) the substring method extracts characters from indexA to indexB without including it
+        //     string.substring(indexA, indexB)
+        // Since the index of the plus sign(atIndex) is the character that should not be included and not the character before it
+        // Example: a.d.d.r.e.s.s+alias@domain.com -> address+alias@domain.com
+        aux[0] =
+            atIndex < 0
+                ? aux[0].replace('.', '')
+                : aux[0].replace('.', '').substring(0, atIndex);
+        return aux.join('@');
+    }
+
+    constructor() {
+        this.data = {};
+    }
+
+    validateValueObject(field, value, required, type, transform) {
+        if (required) {
+            this.constructor.validateRequiredField(field, value);
+        }
+        if (type in this.constructor.validateValueObjectMethodsByType) {
+            this.constructor.validateValueObjectMethodsByType[type](
+                field,
+                value
+            );
+        } else {
+            throw new Error(`type "${type}" is not supported`);
+        }
+        if (type in this.constructor.transformationsValueObjectMethodsByType) {
+            value = this.constructor.transformationsValueObjectMethodsByType[
+                type
+            ](value);
+        }
+        if (typeof transform === 'function') {
+            value = transform(value);
+        }
+        this.data[field] = value;
+        this.defineGetter(field);
+    }
+
+    equals(obj) {
+        if (typeof obj !== 'object') return false;
+        if (!(obj instanceof this.constructor)) return false;
+        var fields = Object.keys(this.data);
+        for (var x = 0; x < fields.length; x++) {
+            if (!Object.is(obj[fields[x]], this[fields[x]])) return false;
+        }
+        return true;
+    }
+
+    defineGetter(field) {
+        Object.defineProperty(this, field, {
+            get: function () {
+                return this.data[field];
+            }
+        });
+    }
+
+    toJSON() {
+        return {
+            ...this.data
+        };
+    }
+}
+
+ValueObject.validateValueObjectMethodsByType = {
+    string: ValueObject.validateStringValueObject,
+    email: ValueObject.validateEmailValueObject,
+    number: ValueObject.validateNumberValueObject,
+    float: ValueObject.validateNumberValueObject,
+    integer: ValueObject.validateIntegerValueObject
+};
+
+ValueObject.transformationsValueObjectMethodsByType = {
+    number: Number,
+    float: Number,
+    integer: Number,
+    email: ValueObject.normalizeEmailValueObject
+};
+
+module.exports = ValueObject;
