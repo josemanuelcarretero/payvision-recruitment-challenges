14155dd1cfc599932797227ae1741a1f5f89e54d
Jose Manuel Carretero <josemanuelcarreterocuenca@gmail.com>
2020-09-16 01:14:30 +0200

    Add the order repository interface and the order use case

---
+++ src/domain/repositories/order.repository.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = class {
+    add(order) {
+        throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+    }
+
+    addAll(order) {
+        throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+    }
+
+    getAll() {
+        throw new Error('ERR_METHOD_NOT_IMPLEMENTED');
+    }
+};
